e775656498c1ac4e6aabcaf19b45e9e029346960
Fernandes <nathalia.fernandes@sap.com>
2020-09-25 11:32:05 -0300

    Adding initial settings

--- src/test/java/constants/ProjectConstants.java
+++ src/test/java/constants/ProjectConstants.java
@@ -1,4 +1,9 @@
 package constants;
 
+import java.io.File;
+
 public class ProjectConstants {
+    public static final String CURRENT_PATH = new File("").getAbsolutePath();
+    public static final String REPORT_PATH = CURRENT_PATH + "/reports/";
+    public static final String URL = "http://automationpractice.com/index.php";
 }
