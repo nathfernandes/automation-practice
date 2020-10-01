fa14887269ae5b5e493c5fff3eb731c4a2377b73
Fernandes <nathalia.fernandes@sap.com>
2020-10-01 13:00:33 -0300

    Removing unused imports

--- src/test/java/pages/Payment/Payment.java
+++ src/test/java/pages/Payment/Payment.java
@@ -4,7 +4,6 @@ import configuration.PageBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import utils.CommonMethods;
 
 import java.util.List;
